e6fef4d9935a58dc4c1325c91916dedcade72cf5
ethan <ethan.gonzalez00774@gmail.com>
2022-09-22 07:45:53 -0700

    objects excersise

---
+++ javascript/triangle-bonus.js
@@ -0,0 +1,45 @@
+
+alert("wuuuu");
+
+//Write a JavaScript function that accepts the 3 lengths of a triangle and outputs the triangle’s area.
+
+// function yourArea = (num1,num2,num3){
+//
+//    let yourTri = (num1 + num2 + num3) ;
+//
+//
+//    return yourTri;
+// }
+// console.log(yourArea(8,6,5));
+//
+
+
+//   A= 1/2 * B * H
+
+//Write a JS function that accepts a string and returns the string in reverse.
+// example input: codeup, example output: puedoc
+
+
+let myArray = ["codeup"];
+
+console.log(myArray);
+
+
+function newArray () {
+
+   let rev = myArray.revers;
+
+   return rev;
+
+}
+
+console.log(newArray());
+
+
+
+
+
+
+
+
+

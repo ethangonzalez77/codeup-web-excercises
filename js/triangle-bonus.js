4e48405fd2d8b71eb6a0c2da37f14e491b28dae5
ethan <ethan.gonzalez00774@gmail.com>
2022-09-26 11:32:26 -0700

    3rd html excercise

--- js/triangle-bonus.js
+++ js/triangle-bonus.js
@@ -175,9 +175,26 @@ function calculateTax(totalpaid, taxPercent) {
     }
    // console.log(some);  log + tab
 }
-
-
-
+//__________________________________________________________
+/**
+Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
+example input: [1,2,3,4,5] expected output: [2,4]
+*/
+let myArray = [1,2,3,4,5,6,7,8,9,10];
+function findEvenNum () {
+    let a2 = myArray[1];
+    let a4 = myArray[3];
+    let a6 = myArray[5];
+    let a8 = myArray[7];
+    let a10 = myArray[9];
+    let result = [a2,a4,a6,a8,a10];
+    return result;
+}
+/**
+Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+    example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+ */
 
 
 
